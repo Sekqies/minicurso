143b96006a56e252d8e7444e1151fb11e1406ddd
Sekqies <fernando.ellerbrock@gmail.com>
2025-11-11 14:29:55 -0300

    segundo cara

--- src/components/SubFrameA.tsx
+++ src/components/SubFrameA.tsx
@@ -35,7 +35,7 @@ const SubFrameA: FunctionComponent = () => {
                   className={styles.googleSymbolsvgIcon}
                   loading="eager"
                   alt=""
-                  src="/google-symbolsvg.svg"
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/768px-Google_%22G%22_logo.svg.png"
                 />
               </div>
               <h1 className={styles.signIn}>
